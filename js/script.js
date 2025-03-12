4cc9e144b816454bd9a59fda421c7e56bf66ef7c
Thayanne Soares <133281353+MisterEu09@users.noreply.github.com>
2025-03-12 18:03:23 -0300

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const pipe = document.querySelector('.pipe');
     const gameOverScreen = document.querySelector('.game-over-screen');
     const restartButton = document.getElementById('restart-button');
+    const scoreDisplay = document.querySelector('.score');
     let loop;
+    let score = 0;
 
     const jump = () => {
         mario.classList.add('jump');
@@ -11,8 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
             mario.classList.remove('jump');
         }, 400);
     };
-    const scoreDisplay = document.querySelector('.score');
-    let score = 0;
 
     const updateScore = () => {
         score++;
@@ -30,9 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
             marioRect.top < pipeRect.bottom
         ) {
             gameOver();
-        }
-        else {
-            updateScore(); // Atualiza a pontuação se não houver colisão
+        } else {
+            updateScore();
         }
     };
 
@@ -42,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         mario.style.animation = 'none';
         mario.src = 'src/mario-jump-images/game-over.png';
         mario.style.width = '50px';
-        gameOverScreen.style.display = 'block'; // Mostra a tela de game over
+        gameOverScreen.style.display = 'block';
     };
 
     const restartGame = () => {
@@ -50,23 +49,22 @@ document.addEventListener('DOMContentLoaded', () => {
         mario.src = 'src/mario-jump-images/mario.gif';
         mario.style.width = '100px';
         mario.style.animation = '';
-    
-        // Reposiciona o cano de volta à posição inicial
         pipe.style.right = '-80px';
-    
-        // Reinicia a animação do cano
-        pipe.style.animation = 'none'; // Pausa a animação
-        void pipe.offsetWidth; // Força uma reflow
-        pipe.style.animation = 'pipe-animation 1.2s infinite linear'; // Reinicia a animação
-    
+        pipe.style.animation = 'none';
+        void pipe.offsetWidth;
+        pipe.style.animation = 'pipe-animation 1.2s infinite linear';
         loop = setInterval(checkCollision, 10);
-
-        score = 0; // Reseta a pontuação
+        score = 0;
         scoreDisplay.textContent = 'Score: 0';
     };
-    
 
     loop = setInterval(checkCollision, 10);
+
+    // Eventos para desktop (teclado)
     document.addEventListener('keydown', jump);
+
+    // Eventos para mobile (toque)
+    document.addEventListener('touchstart', jump);
+
     restartButton.addEventListener('click', restartGame);
 });
